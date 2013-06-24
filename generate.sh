ae45e757b41f78e4e59c9c03f0431a93f0a57253
Jonathan Slate <jonathan@ourstage.com>
2013-06-24 18:21:57 -0400

    fixing MIT years

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
+#phantomjs rasterize.js cover-letter.svg cover-letter.pdf letter
 phantomjs rasterize.js page1.svg page1.pdf letter
 phantomjs rasterize.js page2.svg page2.pdf letter
-pdftk page1.pdf page2.pdf cat output jslate-resume.pdf
-rm page1.pdf page2.pdf
+pdftk  page1.pdf page2.pdf cat output jslate-resume.pdf
+rm  page1.pdf page2.pdf
 open jslate-resume.pdf
